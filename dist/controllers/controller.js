fd1c56b6d616fd05561ea90293795d66574cd7ef
camiloberrio <camiloberriouribe@gmail.com>
2021-05-27 18:18:21 -0500

    Order gitignore

--- dist/controllers/controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sumar = void 0;
-const sumar = (num1, num2) => {
-    return num1 + num2;
-};
-exports.sumar = sumar;
